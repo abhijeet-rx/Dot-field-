1472efa2970ddce1308c2914345f5014091b5a1c
Abhijeet Singh <abhijeetrajput216@gmail.com>
2026-08-29 16:49:50 +0530

    chore: configure PostgreSQL and application entry point

---
+++ dot-field-backend/src/main/java/com/dotfield/DotFieldApplication.java
@@ -0,0 +1,13 @@
+package com.dotfield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotFieldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DotFieldApplication.class, args);
+    }
+
+}
